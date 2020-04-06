5ea855e4c9a4ca4da8d478b141793307a8786b46
Janayna Moura <fernandesmjanayna@gmail.com>
2020-04-06 13:39:14 -0300

    inicializando estudos de front end

---
+++ app/js/models/Negociacao.js
@@ -0,0 +1,29 @@
+class Negociacao {
+    // private _data: Date;
+    // private _quantidade: number;
+    // private _valor: number;
+    //
+    // constructor(data: Date, quantidade: number, valor: number) {
+    //
+    //     this._data = data;
+    //     this._quantidade = quantidade;
+    //     this._valor = valor;
+    // }
+    constructor(_data, _quantidade, _valor) {
+        this._data = _data;
+        this._quantidade = _quantidade;
+        this._valor = _valor;
+    }
+    get data() {
+        return this._data;
+    }
+    get quantidade() {
+        return this._quantidade;
+    }
+    get valor() {
+        return this._valor;
+    }
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+}
